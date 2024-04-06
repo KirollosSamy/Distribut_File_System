fbee4954f57ec8da285843f83d938e2e8fcaf62f
Nu3man11 <76150639+AbdelrahmanNoaman@users.noreply.github.com>
2024-04-06 16:49:51 +0200

    creating lookup tables

--- master/master.go
+++ master/master.go
@@ -1,8 +1,30 @@
 package main
 
-import (
-	// pb "distributed_file_system/grpc/master_node"
-)
+// pb "distributed_file_system/grpc/master_node"
+
+//We need to define lookup tables
+//first one for files
+type FileData struct {
+    filePath string
+    dataKeeperId int
+}
+
+fileLookupTable := make(map[string][]FileData)
+
+//Then define lookup table for nodes
+type NodesData struct {
+    tcpUploadPort string
+	tcpDownloadPort string
+    grpcPort string
+	ip string
+	isAlive bool
+}
+
+NodesLookupTable := make(map[uint32]NodesData)
+
+
+
+
 
 func main() {
 
